6f64075d93f50bc89e1c2ab203e80834fffacc38
dimachep1408 <dmitriychep2011@gmail.com>
2024-06-03 12:23:09 +0300

    Your commit message

---
+++ main_page/app.py
@@ -0,0 +1,9 @@
+import flask
+
+
+main = flask.Blueprint(
+    name = "main",
+    import_name= "app",
+    template_folder= "main_page/templates",
+    static_folder= "main_page"
+)
---
+++ main_page/views.py
@@ -0,0 +1,5 @@
+import flask
+
+def render_main():
+
+    return flask.render_template(template_name_or_list= "main.html")
